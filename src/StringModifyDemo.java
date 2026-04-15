26eb36541d35ee7c62273ee444bfda051c31b008
Shahd <shahad9mubarak@gmail.com>
2026-04-15 12:29:09 +0400

    Task 3: Search and Modify Text Using Java String Methods

---
+++ src/StringModifyDemo.java
@@ -0,0 +1,11 @@
+public class StringModifyDemo {
+    static void main(String[] args) {
+        String Course= ("Satellite Imagery Processing");
+        System.out.println(Course);
+        System.out.println(Course.trim());
+        System.out.println(Course.contains("Satellite"));
+        System.out.println(Course.replace("Satellite","Landsat"));
+
+
+    }
+}
