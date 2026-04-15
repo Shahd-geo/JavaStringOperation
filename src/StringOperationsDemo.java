6c8576972c2f8a4a0bb590e024adb8f87bdea7ca
Shahd <shahad9mubarak@gmail.com>
2026-04-15 12:15:48 +0400

    Task 1: Create and Perform Basic Java String Operations

---
+++ src/StringOperationsDemo.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+
+public class StringOperationsDemo {
+    static void main(String[] args) {
+        String CustmoerName = "Shahd";
+        System.out.println(CustmoerName.length());
+        System.out.println(CustmoerName.toUpperCase());
+        System.out.println(CustmoerName.toLowerCase());
+        String COMPANY = ("PDO");
+        System.out.println( CustmoerName.concat(COMPANY));
+
+    }
+
+}
+
+
