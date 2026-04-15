1c4b5e17ad37214805477b12a4668e1060fd54a4
Shahd <shahad9mubarak@gmail.com>
2026-04-15 14:29:57 +0400

    Task 7: Compare and Analyze Text Using Java String Methods

---
+++ src/StringAnalysisDemo.java
@@ -0,0 +1,14 @@
+public class StringAnalysisDemo {
+    static void main(String[] args) {
+        String MOTIVATION = ("work hard");
+        System.out.println(MOTIVATION);
+        System.out.println(MOTIVATION.length());
+        String MOTIVATION2 = ("well done");
+        System.out.println(MOTIVATION.equals(MOTIVATION2));
+        System.out.println(MOTIVATION.indexOf("h"));
+        System.out.println(MOTIVATION.startsWith("work"));
+        System.out.println(MOTIVATION.endsWith("d"));
+
+
+    }
+}
