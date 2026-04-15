b715b007ef56b3120d21e9a3ac3e7b255ee4c5c5
Shahd <shahad9mubarak@gmail.com>
2026-04-15 14:33:56 +0400

    Task 8: Convert, Search, and Extract Data Using Java String Method

---
+++ src/StringDataDemo.java
@@ -0,0 +1,11 @@
+public class StringDataDemo {
+    static void main(String[] args) {
+        String MASSAGE = ("GOOD MORNING SUN");
+        System.out.println(MASSAGE);
+        System.out.println(MASSAGE.toUpperCase());
+        System.out.println(MASSAGE.toLowerCase());
+        System.out.println(MASSAGE.indexOf("S"));
+        System.out.println(MASSAGE.substring(1,6));
+        System.out.println(MASSAGE.contains("oo"));
+    }
+}
