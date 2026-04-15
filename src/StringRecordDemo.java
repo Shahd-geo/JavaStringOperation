0ba89edce8eea781e744adcd940d4932a12bf283
Shahd <shahad9mubarak@gmail.com>
2026-04-15 14:39:32 +0400

    Task 9: Compare, Clean, and Split Text Using Java String Methods

---
+++ src/StringRecordDemo.java
@@ -0,0 +1,16 @@
+public class StringRecordDemo {
+    static void main(String[] args) {
+        String MOTIVATION = (" work hard ");
+        System.out.println(MOTIVATION);
+        System.out.println(MOTIVATION.trim());
+        String TEXT = ("HELLO");
+        String TEXT2 = ("EHLLO");
+        System.out.println(TEXT.equalsFoldCase(TEXT2));
+       System.out.println( TEXT2.split(","));
+       String EM ="";
+       System.out.println(EM.isEmpty());
+
+
+
+    }
+}
