9485db8f628d0ec99a604b1b38599623abb6e87d
Shahd <shahad9mubarak@gmail.com>
2026-04-15 12:22:58 +0400

    Task 2: Extract and Compare Text Using Java String Methods

---
+++ src/StringCompareDemo.java
@@ -0,0 +1,13 @@
+public class StringCompareDemo {
+    static void main(String[] args) {
+        String Course= ("Geospatial");
+        System.out.println(Course);
+        System.out.println(Course.charAt(5));
+        System.out.println(Course.substring(3,7));
+        String Course2= ("Analysis");
+        System.out.println(Course.equals(Course2));
+       System.out.println(Course + " "+Course2);
+
+
+    }
+}
